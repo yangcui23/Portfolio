6a33b3f41fb412617c46eb5cc1efb19150817e71
Yang Cui <yangcui283@yahoo.com>
2023-06-26 23:32:42 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -125,14 +125,28 @@ function App() {
                 }
             })
         })
+        const observer5 = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+
+                console.log(entries)
+                if (entry.isIntersecting) {
+
+                    entry.target.classList.add('show5');
+                } else {
+                    entry.target.classList.remove('show5');
+                }
+            })
+        })
     const hiddenElements = document.querySelectorAll('.tweetly');
     const hiddenElements2 = document.querySelectorAll('.centura');
         const hiddenElements3 = document.querySelectorAll('.coming');
         const hiddenElements4 = document.querySelectorAll('.coming2');
+        const hiddenElements5 = document.querySelectorAll('.coming3');
     hiddenElements.forEach((el) => observer.observe(el));
     hiddenElements2.forEach((el) => observer2.observe(el));
         hiddenElements3.forEach((el) => observer3.observe(el));
         hiddenElements4.forEach((el) => observer4.observe(el));
+        hiddenElements5.forEach((el) => observer5.observe(el));
     })
 
 
@@ -169,7 +183,7 @@ function App() {
 
                     <h3 className="pro text-3xl text-neutral-300">const Projects = ( ) => {'{'}</h3>
                 </div>
-            <a href="http://www.tweetly.org" >
+            <button>
                 <div className="tweetly" >
                     <div className="wrapper">
 
@@ -185,8 +199,8 @@ function App() {
 
 
                 </div>
-            </a>
-                <a href="http://www.allcentura.com">
+            </button>
+               <button>
 
                 <div className="centura mt-12" >
                     <div className="wrapper">
@@ -203,9 +217,8 @@ function App() {
 
 
                 </div>
-                </a>
-                <a href="#">
-
+               </button>
+               <button>
                     <div className="coming mt-12" >
                         <div className="wrapper">
 
@@ -221,8 +234,8 @@ function App() {
 
 
                     </div>
-                </a>
-                <a href="#">
+               </button>
+                <button>
 
                     <div className="coming2 mt-12" >
                         <div className="wrapper">
@@ -239,7 +252,25 @@ function App() {
 
 
                     </div>
-                </a>
+                </button>
+                <button>
+
+                    <div className="coming3 mt-12" >
+                        <div className="wrapper">
+
+                            <div className="projectText">
+
+                                <p className="cardText">TBD</p>
+                                <div className="textUnderline"></div>
+                            </div>
+
+                        </div>
+
+                        <img className="projectImg" src="./src/assets/coming.jpg" alt="project website"/>
+
+
+                    </div>
+                </button>
                 <p className="rightClose text-neutral-400 text-3xl">{'}'}</p>
             </div>
             <div id="section3" className="about">
@@ -427,7 +458,7 @@ function App() {
                                             onChange={(e) => setMessage(e.target.value)}
                                         ></textarea>
                                         {errorMessage && <div className="error">{errorMessage}</div>}
-                                        <button type="submit" className="button-56">Send Message</button>
+                                        <button type="submit" className="button-89" role="button">Send Message</button>
                                         {showSuccessMessage && <p>Email sent successfully!</p>}
                                     </form>
                                 </div>
@@ -453,7 +484,47 @@ function App() {
         </div>
         <style>
             {`
+            
+
+
+.button-89 {
+margin-top : 20px;
+  --b: 3px;   
+  --s: .45em; 
+  --color: #373B44;
+  
+  padding: calc(.5em + var(--s)) calc(.9em + var(--s));
+  color: var(--color);
+  --_p: var(--s);
+  background:
+    conic-gradient(from 90deg at var(--b) var(--b),#0000 90deg,var(--color) 0)
+    var(--_p) var(--_p)/calc(100% - var(--b) - 2*var(--_p)) calc(100% - var(--b) - 2*var(--_p));
+  transition: .3s linear, color 0s, background-color 0s;
+  outline: var(--b) solid #0000;
+  outline-offset: .6em;
+  font-size: 16px;
+
+  border: 0;
+
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-89:hover,
+.button-89:focus-visible{
+  --_p: 0px;
+  outline-color: var(--color);
+  outline-offset: .05em;
+}
+
+.button-89:active {
+  background: var(--color);
+  color: #fff;
+}
+                 
             .form {
+            
             position: fixed;
             top: 0;
             left: 0;
@@ -473,13 +544,16 @@ function App() {
           justify-content:center;
           align-items:center;
           margin-top : 200px;
-          margin-left : 570px;
+          margin-left : 600px;
           border-radius:10px;
             height:400px;
             width:700px;
             background-color: #d9d9d9;
-            padding: 20px;
+            padding: 30px;
             transition: all 0.3s ease-out; 
+            align-items: center;
+            justify-content: center;
+            text-align:center;
   
           }
             
@@ -501,6 +575,7 @@ function App() {
             align-items: center;
             justify-content: center;
             z-index: 999;
+            
           }
 
           .modal {
@@ -509,6 +584,8 @@ function App() {
             width:300px;
             background-color: #d9d9d9;
             padding: 20px;
+            
+            
           }
 
           .close-button {
